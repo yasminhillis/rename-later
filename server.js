b571840aa0927c1bbfbefdecfe582aa25c27718c
Yasmin Hillis <jasminehillis94@gmail.com>
2021-02-16 18:47:05 +0200

    route setup

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ app.get('/', (req, res) => res.send('home route'));
 app.use('/api/users', require('./routes/api/users'));
 
 app.listen(PORT, () => console.log(`app running on port ${PORT}`));
+
+app.use('/api/profile', require('./routes/api/profile'));
+
+app.use('/api/posts', require('./routes/api/posts'));
+
+app.use('/api/auth', require('./routes/api/auth'));
